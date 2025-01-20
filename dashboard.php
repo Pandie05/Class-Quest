c71145a8978b918188c93e0c62cd7d201e86167e
Pandie05 <hreyes@email.neit.edu>
2025-01-20 15:30:09 -0500

    wooooooooooooo

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,58 @@
 <?php
-$skibidi = 1;
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Quest</title>
+
+</head>
+<body>
+
+    <nav>
+        
+        <div class="logo"></div>
+
+        <div class="add-btn"><div>
+
+        <div class="icon-links">
+            
+            <h1></h1>
+            
+            <h1></h1>
+
+            <div class="pet-board">
+
+                <div class="pet-cont"></div>
+
+                <div class="pet-stats"></div>
+
+            </div>
+
+            <div class="assignments">
+
+            </div>
+        
+        <div>
+
+        <div class="logout"></div>
+        
+    </nav>
+
+    <div>
+        
+
+
+    <div>
+
+    <div>
+        
+    
+    
+    <div>
+    
+</body>
+</html>
