2b9b1911a2a01d879d5a2df31b74f801c7e9cc28
judothin <fagrejayden@gmail.com>
2025-02-08 00:18:50 -0500

    profile page mobile styling done (looks meh)

--- profile.php
+++ profile.php
@@ -95,7 +95,7 @@
                     </div>
 
                     <div class="password-input">
-                        <label for="new-password">New Password:</label>
+                        <label for="new-password">New PW:</label>
                         <div style="position: relative;">
                             <input type="password" name="new-password" id="new-password" placeholder="New Password" style="padding-right: 30px;">
                             <svg id="toggleNewPassword" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); cursor: pointer;">
@@ -105,13 +105,14 @@
                     </div>
 
                     <div class="password-input">
-                        <label for="confirm-password">Confirm Password:</label>
+                        <label for="confirm-password">Confirm PW:</label>
                         <div style="position: relative;">
                             <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" style="padding-right: 30px;">
                             <svg id="toggleConfirmPassword" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); cursor: pointer;">
                                 <path fill="currentColor" d="M12 9a3 3 0 0 1 3 3a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3m0-4.5c5 0 9.27 3.11 11 7.5c-1.73 4.39-6 7.5-11 7.5S2.73 16.39 1 12c1.73-4.39 6-7.5 11-7.5M3.18 12a9.821 9.821 0 0 0 17.64 0a9.821 9.821 0 0 0-17.64 0"/>
                             </svg>
                         </div>
+                        
                     </div>
 
                     <button id="change-password-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-dasharray="24" stroke-dashoffset="24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11l6 6l10 -10"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="24;0"/></path></svg></button>
