1aa222bb832a87ae6d40650a1ee8578d689399ef
Pandie05 <hreyes@email.neit.edu>
2025-02-10 23:35:27 -0500

    fixed PW bug
    removed password view on user page

--- profile.php
+++ profile.php
@@ -92,18 +92,9 @@
                 <p>Email: <?php echo $_SESSION['user']['email']; ?></p>
                 
                 <div class="password-change-section">
-                    <div class="password-input">
-                        <label for="password">Password:</label>
-                        <div style="position: relative;">
-                            <input type="password" name="password" id="password" placeholder="Password" value="<?php echo $_SESSION['user']['password']; ?>" readonly style="padding-right: 30px;">
-                            <svg id="togglePassword" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); cursor: pointer;">
-                                <path fill="currentColor" d="M12 9a3 3 0 0 1 3 3a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3m0-4.5c5 0 9.27 3.11 11 7.5c-1.73 4.39-6 7.5-11 7.5S2.73 16.39 1 12c1.73-4.39 6-7.5 11-7.5M3.18 12a9.821 9.821 0 0 0 17.64 0a9.821 9.821 0 0 0-17.64 0"/>
-                            </svg>
-                        </div>
-                    </div>
 
                     <div class="password-input">
-                        <label for="new-password">New PW:</label>
+                        <label for="new-password">New Password:</label>
                         <div style="position: relative;">
                             <input type="password" name="new-password" id="new-password" placeholder="New Password" maxlength="20" style="padding-right: 30px;">
                             <svg id="toggleNewPassword" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); cursor: pointer;">
@@ -113,7 +104,7 @@
                     </div>
 
                     <div class="password-input">
-                        <label for="confirm-password">Confirm PW:</label>
+                        <label for="confirm-password">Confirm Password:</label>
                         <div style="position: relative;">
                             <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" maxlength="20" style="padding-right: 30px;">
                             <svg id="toggleConfirmPassword" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="position: absolute; right: 5px; top: 50%; transform: translateY(-50%); cursor: pointer;">
